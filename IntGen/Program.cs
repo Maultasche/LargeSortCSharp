c03f39ab9c4c2e18b76780c2ce0fad0e0756b242
Maultasche <kevin.peter@gmail.com>
2018-06-14 14:55:12 -0700

    Implemented a progress bar to track the progress of integer generation

--- IntGen/Program.cs
+++ IntGen/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 using CommandLine;
 using Microsoft.Extensions.DependencyInjection;
+using ShellProgressBar;
 
 using LargeSort.Shared.Interfaces;
 using LargeSort.Shared.Implementations;
@@ -24,12 +26,62 @@ namespace IntGen
                 //Parse the arguments from the command line and act in accordance to whether they were parsed
                 //successfully or not
                 var parseResult = CommandLine.Parser.Default.ParseArguments<CommandLineOptions>(args)
-                    .WithParsed(options => GenerateIntegers(options))
+                    .WithParsed(options =>
+                    {
+                        //Create the progress bar
+                        var progressBar = CreateProgressBar(options.Count);
+
+                        //Generate the integers and pass a method that will update the progress bar
+                        GenerateIntegers(options, generatedIntegers => UpdateProgressBar(progressBar, generatedIntegers));
+
+                        GenerateIntegers(options, generatedIntegers => { });
+
+                        Console.WriteLine();
+                    })
                     .WithNotParsed(errors => Environment.ExitCode = -1);
             }
             catch(Exception exception)
             {
                 Console.WriteLine(exception.Message);
+            }
+        }
+
+        static ProgressBar CreateProgressBar(uint totalIntegers)
+        {
+            var options = new ProgressBarOptions()
+            {
+                BackgroundCharacter = '\u2593',
+                ProgressBarOnBottom = true,
+                DisplayTimeInRealTime = false,
+                ForegroundColor = ConsoleColor.Gray,
+                ForegroundColorDone = ConsoleColor.Gray,
+                BackgroundColor = ConsoleColor.DarkGray
+            };
+
+            var progressBar = new ProgressBar(Convert.ToInt32(totalIntegers), "Generating Integers...", options);
+
+            return progressBar;
+        }
+
+        static void UpdateProgressBar(ProgressBar progressBar, int generatedIntegers)
+        {
+            //If we update for every integer, and large numbers of integers are being generated, 
+            //the application will more time updating the progress bar than it will doing its real work
+            //So the solution is to update the progress bar every 1% of generated integers.
+            //For a million integers, this reduced update rate reduced the runtime from over 9 minutes 
+            //to under a second
+
+            int tickSize = progressBar.MaxTicks / 100;
+
+            if(generatedIntegers % tickSize == 0)
+            {
+                progressBar.Tick(generatedIntegers);
+
+                if (generatedIntegers == progressBar.MaxTicks)
+                {
+                    progressBar.Message = "Integer Generation Complete";
+                }
+                    
             }
         }
 
@@ -48,7 +100,8 @@ namespace IntGen
             return serviceCollection.BuildServiceProvider();
         }
 
-        static void GenerateIntegers(CommandLineOptions options)
+
+        static void GenerateIntegers(CommandLineOptions options, Action<int> updateIntegerGenerationStatus)
         {
             //Get an instance of the integer file creator
             IIntegerFileCreator integerFileCreator = serviceProvider.GetService<IIntegerFileCreator>();
@@ -60,6 +113,27 @@ namespace IntGen
             //it is enumerated rather than a whole bunch at once
             IEnumerable<int> randomIntegers = randomIntegerGenerator.CreateIntegerGenerator(options.LowerBound, 
                 options.UpperBound, options.Count);
+
+            int integersGenerated = 0;
+
+
+            //When an integer is enumerated, update the progress bar
+            randomIntegers = randomIntegers.Select(integer =>
+            {
+                integersGenerated++;
+
+                //if(integersGenerated == options.Count)
+                //{
+                    updateIntegerGenerationStatus(integersGenerated);
+                //}
+                //else
+                //{
+                //    Task.Run(() => updateIntegerGenerationStatus(integersGenerated));
+                //}
+                
+
+                return integer;
+            });
 
             //Create the integer file with the randomly-generated integers
             integerFileCreator.CreateIntegerTextFile(randomIntegers, options.FilePath);
